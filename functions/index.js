ede4ef4a54e78da2fb4ed46a74040b3bbe18d021
Luca8991 <luca.berte98@hotmail.it>
2021-07-17 16:29:43 +0200

    v1 api slice and logs

--- functions/index.js
+++ functions/index.js
@@ -45,34 +45,49 @@ const app = express();
 // Automatically allow cross-origin requests
 app.use(cors({origin: true}));
 
-app.get("/getFilesystem", async (req, res) => {
-  const userId = req.query.userId;
-  const chat = await bot.telegram.getChat(userId);
-  const fileId = chat.pinned_message.document.file_id;
-  const downloadUrl = await bot.telegram.getFileLink(fileId);
+app.get("/v1/getFilesystem", async (req, res) => {
+  console.log("handling GETFILESYSTEM request");
+  try {
+    const userId = req.query.userId;
+    const chat = await bot.telegram.getChat(userId);
+    const fileId = chat.pinned_message.document.file_id;
+    const downloadUrl = await bot.telegram.getFileLink(fileId);
 
-  // console.log("DOWNLOAD URL", downloadUrl.href);
+    // console.log("DOWNLOAD URL", downloadUrl.href);
 
-  request.get(downloadUrl.href, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
+    request.get(downloadUrl.href, (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        throw new Error(error);
+      }
       const fileSystem = JSON.parse(body);
       const formattedFileSys = [];
       formatFileSys(fileSystem["/"], formattedFileSys, "");
-      console.log(formattedFileSys);
       res.send({data: formattedFileSys});
-    }
-  });
+    });
+  } catch (err) {
+    console.log("ERROR", err);
+    res.status(500).send(err.message);
+  }
+  return;
 });
-app.post("/downloadFile", async (req, res) => {
-  const userId = req.body.userId;
-  const messageId = req.body.messageId;
-  bot.telegram.sendMessage(userId,
-      `Requested file at \`${new Date().toISOString()}\` from website.`,
-      {
-        reply_to_message_id: messageId,
-        parse_mode: "Markdown",
-      });
-  res.sendStatus(200);
+app.post("/v1/downloadFile", async (req, res) => {
+  console.log("handling DOWNLOADFILE request");
+  try {
+    const userId = req.body.userId;
+    const messageId = req.body.messageId;
+    await bot.telegram.sendMessage(userId,
+        `Requested file.\nTime: \`${new Date().toISOString()}\`
+        \nFrom: website.`,
+        {
+          reply_to_message_id: messageId,
+          parse_mode: "Markdown",
+        });
+    res.sendStatus(200);
+  } catch (err) {
+    console.log("ERROR", err);
+    res.status(500).send(err.message);
+  }
+  return;
 });
 
 exports.api = functions.https.onRequest(app);
